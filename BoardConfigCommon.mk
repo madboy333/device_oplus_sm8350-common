TheXPerienceProject/android_device_oplus_sm8350-common

sm8350-common: Add logic for builds with prebuilt dtb/prebuilt kernel * Helpful for bringups and partially closed sources

BoardConfigCommon.mk
@@ -101,11 +101,19 @@ BOARD_KERNEL_CMDLINE := \
     iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
+
+ifndef BOARD_PREBUILT_DTBOIMAGE
 BOARD_KERNEL_SEPARATED_DTBO := true
+endif
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
+
+ifndef TARGET_PREBUILT_KERNEL
 TARGET_KERNEL_SOURCE := kernel/oplus/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
+else
+TARGET_FORCE_PREBUILT_KERNEL := true
+endif
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(COMMON_PATH)/modules.blocklist
@@ -155,7 +163,11 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Recovery
+ifndef TARGET_PREBUILT_DTB
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+else
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+endif
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
 BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/init/fstab.default

madboy333/device_oplus_sm8350-common

BoardConfigCommon.mk
@@ -102,11 +102,17 @@ BOARD_KERNEL_CMDLINE := \
     iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
+
+ifndef BOARD_PREBUILT_DTBOIMAGE
 BOARD_KERNEL_SEPARATED_DTBO := true
+endif
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_SOURCE := kernel/oplus/sm8350
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
+ifdef TARGET_PREBUILT_KERNEL
+TARGET_FORCE_PREBUILT_KERNEL := true
+endif
 TARGET_KERNEL_NO_GCC := true
 
 # Kernel modules
@@ -157,7 +163,11 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Recovery
+ifndef TARGET_PREBUILT_DTB
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+else
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+endif
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
 BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
 TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/init/fstab.default